352247a940730f571afaa2e1fab1180a3b67e2cd
mjan <m.janbugti12@gmail.com>
2022-01-26 02:08:29 +0500

    pre slae styling issues done of hompage

--- src/pages/pre-sale/home.js
+++ src/pages/pre-sale/home.js
@@ -2,6 +2,7 @@ import React from "react"
 import Seo from "../../components/seo"
 
 import Header from "../../components/header"
+import Auction from './auction';
 
 const Home = () => {
     return (
@@ -11,24 +12,39 @@ const Home = () => {
                 <Header />
                 <section className="home-section">
                     <div className="container">
-                        <div className="row">
+                        <div className="row flex-row-reverse">
+                            <div className="col-sm-7 d-flex flex-column justify-content-center desctription">
+                                <h4 className="title1 txt-green">ExcluSive pre-sale of</h4>
+                                <h3 className="title2">NDB TOKEN</h3>
+                                <p className="text">
+                                Don’t miss an opportunity to get ahold of NDB Tokens before anyone 
+                                else at the fixed price before the auction starts.
+                                </p>
+                                {/* Desktop View Button */}
+                                <div className="btn-desktop mt-2">
+                                <div>
+                                    <button className="btn btn-green" onClick={() => {
+                                            // TODO: Handle Moving it component
+                                        }}>Buy</button>
+                                </div>
+                                <div className="col-sm-5"></div>
+                            </div>
+                            </div>
                             <div className="col-sm-5 d-flex justify-content-center align-items-center imageDiv">
                                 <div className="hero-image"></div>
                             </div>
-                            <div className="col-sm-7 d-flex flex-column justify-content-center desctription">
-                                <h4 className="title1 txt-green">Exclusive Sale of</h4>
-                                <h3 className="title2">10 ndb tokens</h3>
-                                <p className="text">
-                                    Don’t miss an opportunity to buy limited number of NDB Tokens at
-                                    the fixed price before the next auction round started.
-                                </p>
-                            </div>
+                            
                         </div>
-                        <div className="row">
-                            <div className="col-sm-7">
-                                <button className="btn btn-green">buy now</button>
+                        {/* Mobile View Button */}
+                        <div className="btn-mobile">
+                                <div className="">
+                                    <button className="btn btn-green" onClick={() => {
+                                            // Handle Moving it 
+                                        }}>Buy</button>
+                                </div>"
+                            <div className="text-decoration-underline">
+                                Learn more
                             </div>
-                            <div className="col-sm-5"></div>
                         </div>
                     </div>
                 </section>
